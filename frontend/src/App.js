2b8d55ca8a5f66aef2e19dfacb4984b54b729fae
alitemel89 <alitemel89.ce@gmail.com>
2022-05-23 18:25:08 +0300

    complete ProductScreen page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,26 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 
 function App() {
   return (
-    <>
-      <Header/>
+    <BrowserRouter>
+      <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
