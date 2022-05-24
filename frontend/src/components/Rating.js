e7668e10be9207cb98efd73e9e3fe6ca6b5d674f
alitemel89 <alitemel89.ce@gmail.com>
2022-05-24 12:02:47 +0300

    setup server nodemon and concurrently

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Rating = ({ value, text, color }) => {
     return (
@@ -58,10 +57,5 @@ Rating.defaultProps = {
     color: '#f8e825'
 }
 
-Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-}
 
 export default Rating;
