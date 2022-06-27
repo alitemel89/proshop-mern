b886c47312e7a3aaa54a99428aa933dd292433d7
alitemel89 <alitemel89.ce@gmail.com>
2022-06-27 21:02:26 +0300

    add login functionality with redux toolkit

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,8 +1,103 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import FormContainer from '../components/FormContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner';
+import { login, reset } from '../services/authSlice';
 
 const LoginScreen = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const handleChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+
   return (
-    <div>LoginScreen</div>
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            className='form-control'
+            id='email'
+            name='email'
+            value={email}
+            placeholder='Enter your email'
+            onChange={handleChange}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password" className="mt-2">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            className='form-control'
+            id='password'
+            name='password'
+            value={password}
+            placeholder='Enter password'
+            onChange={handleChange}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="mt-3">
+          Sign In
+        </Button>
+
+        <Row className="py-3">
+          <Col>
+            New Customer? <Link to="/register"> Register</Link>
+          </Col>
+        </Row>
+      </Form>
+    </FormContainer>
   )
 }
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productsApi } from './services/productsApi'
 import cartReducer from './services/cartSlice';
+import authReducer from './services/authSlice';
+
 
 export default configureStore({
   reducer: {
     [productsApi.reducerPath]: productsApi.reducer,
-    cart: cartReducer
+    cart: cartReducer,
+    auth: authReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({}).concat(productsApi.middleware)
